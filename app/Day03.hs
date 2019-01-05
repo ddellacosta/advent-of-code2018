cb21b7185543271c14f985806e2105516aa451ba
Dave Della Costa <ddellacosta@gmail.com>
2019-01-05 15:56:29 -0500

    whoops, forgot one test I meant to add

--- app/Day03.hs
+++ app/Day03.hs
@@ -80,8 +80,8 @@ claimsWith2orMore = M.foldr (\(c, cms) twoPlus
                                 then S.union cms twoPlus
                                 else twoPlus) S.empty
 
-findIsolatedGridSegments :: String -> String
-findIsolatedGridSegments inputStr = show $ S.difference cms1 cms2plus
+findIsolatedGridSegments :: String -> ClaimNo
+findIsolatedGridSegments inputStr = head $ S.toList $ S.difference cms1 cms2plus
   where claims = readClaims inputStr
         cms1 = claimsWith1 claims
         cms2plus = claimsWith2orMore claims
@@ -154,4 +154,9 @@ ex4 = "#1 @ 0,0: 5x5\n#2 @ 1,1: 5x5\n#3 @ 2,2: 5x5\n#4 @ 3,3: 5x5\n#5 @ 4,4: 5x5
 -- |
 -- >>> countOverlaps $ readClaims ex4
 -- 37
+--
+
+-- |
+-- >>> findIsolatedGridSegments $ ex4
+-- "6"
 --
